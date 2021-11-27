87ca5d7848f6b5e46550de52354a4a7903e65d82
Nadhir <nadhirxz@gmail.com>
2021-11-27 22:24:29 +0100

    Getting version and description from package.json

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { fileExists } from './utils/files';
 import { findHost, hosts } from './utils/hosts';
 import run from './utils/run';
 import { bold, underline } from 'colors';
+require('pkginfo')(module);
 
 program
-	.version('0.1.0')
-	.description('Simple Node.js CLI tool to upload files')
+	.version(module.exports.version)
+	.description(module.exports.description)
 	.argument('<filename>', 'file to upload')
 	.addOption(new Option('-h, --host <host>', 'hosting service').choices(hosts.map(e => e.name)).default(findHost('default', true).name))
 	.option('-nc, --no-copy', 'disable copying file url to clipboard after upload')
